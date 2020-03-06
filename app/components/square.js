160e244155c70ad70c644d55fc37e6a018df7af3
Jay Gruber <jay.gruber@avalara.com>
2020-03-06 00:24:26 -0800

    added test cases, hover border

--- app/components/square.js
+++ app/components/square.js
@@ -1,5 +1,37 @@
 import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
 
 export default class SquareComponent extends Component {
-  isEmpty = false;
+  /**
+   * component arguments
+   *
+   * required
+   * @property row {Number} - the y coordinate of this square in the game board
+   * @property column {Number} - the x coordinate of this square in the game board
+   * @property ships {Array} - a [row] x [column] 2d array
+   *
+   * optional
+   * @property shouldShowShip {Boolean}(default = false)
+   */
+
+  constructor() {
+    super(...arguments);
+    const { ships, row, column } = this.args;
+    assert('row {Number} is required', Number.isInteger(row));
+    assert('column {Number} is required', Number.isInteger(column));
+    assert('ships {Array} is required', Array.isArray(ships));
+  }
+
+  get state() {
+    const { shouldShowShip, ships, shots, row, column } = this.args;
+    const ship = ships[row][column];
+    const shot = shots[row][column];
+    return shouldShowShip ? shot || ship : shot || 0;
+  }
+
+  get background() {
+    // [empty, ship, miss, hit]
+    const backgroundClasses = ['bg-blue-300', 'bg-gray-400', 'bg-white', 'bg-red-500'];
+    return backgroundClasses[this.state];
+  }
 }
--- tests/integration/components/square-test.js
+++ tests/integration/components/square-test.js
@@ -1,24 +1,82 @@
+import { render } from '@ember/test-helpers';
+import { setProperties } from '@ember/object';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | square', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    await render(hbs`
-      <table>
-        <tbody>
-          <tr>
-            <Square
-            />
-          </tr>
-        </tbody>
-      </table>
-    `);
+  const stateEnum = {
+    empty: 0,
+    ship: 1,
+    miss: 2,
+    hit: 3
+  };
 
-    assert.dom('td').hasText('X', 'it has an X when it is non-empty');
-    await this.pauseTest();
+  const classes = ['bg-blue-300', 'bg-gray-400', 'bg-white', 'bg-red-500'];
+
+  const testCases = [
+    {
+      name: 'empty',
+      expectedClass: classes[stateEnum.empty],
+      row: 0,
+      column: 0,
+      ships: [[0]],
+      shots: [[0]],
+      shouldShowShip: false
+    },
+    {
+      name: 'ship, shouldShowShip=false, miss',
+      expectedClass: classes[stateEnum.miss],
+      row: 0,
+      column: 0,
+      ships: [[1]],
+      shots: [[2]],
+      shouldShowShip: false
+    },
+    {
+      name: 'ship, shouldShowShip=false, hit',
+      expectedClass: classes[stateEnum.hit],
+      row: 0,
+      column: 0,
+      ships: [[1]],
+      shots: [[3]],
+      shouldShowShip: false
+    },
+    {
+      name: 'ship, shouldShowShip=true, miss',
+      expectedClass: classes[stateEnum.miss],
+      row: 0,
+      column: 0,
+      ships: [[1]],
+      shots: [[2]],
+      shouldShowShip: true
+    },
+    {
+      name: 'ship, shouldShowShip=true, hit',
+      expectedClass: classes[stateEnum.hit],
+      row: 0,
+      column: 0,
+      ships: [[1]],
+      shots: [[3]],
+      shouldShowShip: true
+    }
+  ];
+  testCases.forEach(({ name, expectedClass, ...props }) => {
+    test(name, async function(assert) {
+      setProperties(this, props);
+      await render(hbs`
+        <Square
+          @row={{this.row}}
+          @column={{this.column}}
+          @ships={{this.ships}}
+          @shots={{this.shots}}
+          @shouldShowShip={{this.shouldShowShip}}
+        />
+      `);
+
+      assert.dom('td').hasClass(expectedClass);
+    });
   });
 });
