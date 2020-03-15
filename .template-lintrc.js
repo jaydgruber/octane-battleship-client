951d17cc20c4afbc46277e6f8575625eb52d1f84
Jay Gruber <jay.gruber@avalara.com>
2020-03-14 18:14:01 -0700

    fixed all lint and tests. added npm-run-all. changes npm scripts

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   /**
-  * cannot use prettier w/ template-lint 2.0 yet
-  * ref: https://github.com/ember-template-lint/ember-template-lint/issues/1150
-  */
+   * cannot use prettier w/ template-lint 2.0 yet
+   * ref: https://github.com/ember-template-lint/ember-template-lint/issues/1150
+   */
   // plugins: ["ember-template-lint-plugin-prettier"],
   // extends: ["recommended", "ember-template-lint-plugin-prettier:recommended"],
 
-  extends: ["octane", "stylistic"],
+  extends: ['octane', 'stylistic']
 };
